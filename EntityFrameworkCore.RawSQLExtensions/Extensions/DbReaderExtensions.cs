3ec315d4a4c76e1811634e5a66a42b4ab4c102a1
MysticBoy <mysticboy@live.com>
2018-01-18 01:04:57 +0800

    fix some error

--- EntityFrameworkCore.RawSQLExtensions/Extensions/DbReaderExtensions.cs
+++ EntityFrameworkCore.RawSQLExtensions/Extensions/DbReaderExtensions.cs
@@ -12,7 +12,7 @@ namespace EntityFrameworkCore.RawSQLExtensions.Extensions
         public static IDictionary<string, DbColumn> GetSchema<T>(this DbDataReader dr)
         {
             var props = typeof(T).GetRuntimeProperties();
-            
+
             return dr.GetColumnSchema()
                     .Where(x => props.Any(y => y.Name.ToLower() == x.ColumnName.ToLower()))
                     .ToDictionary(key => key.ColumnName.ToLower());
@@ -48,13 +48,26 @@ namespace EntityFrameworkCore.RawSQLExtensions.Extensions
 
         public static async Task<IList<T>> ToListAsync<T>(this DbDataReader dr)
         {
-            var objList = new List<T>();     
-            
+            var objList = new List<T>();
+
             var props = typeof(T).GetRuntimeProperties();
             var colMapping = dr.GetSchema<T>();
 
             if (dr.HasRows)
                 while (await dr.ReadAsync())
+                    objList.Add(dr.MapObject<T>(colMapping, props));
+
+            return objList;
+        }
+        public static IList<T> ToList<T>(this DbDataReader dr)
+        {
+            var objList = new List<T>();
+
+            var props = typeof(T).GetRuntimeProperties();
+            var colMapping = dr.GetSchema<T>();
+
+            if (dr.HasRows)
+                while (dr.Read())
                     objList.Add(dr.MapObject<T>(colMapping, props));
 
             return objList;
@@ -67,6 +80,18 @@ namespace EntityFrameworkCore.RawSQLExtensions.Extensions
 
             if (dr.HasRows)
                 while (await dr.ReadAsync())
+                    return dr.MapObject<T>(colMapping, props);
+
+            return default(T);
+        }
+
+        public static T FirstOrDefault<T>(this DbDataReader dr)
+        {
+            var props = typeof(T).GetRuntimeProperties();
+            var colMapping = dr.GetSchema<T>();
+
+            if (dr.HasRows)
+                while (dr.Read())
                     return dr.MapObject<T>(colMapping, props);
 
             return default(T);
@@ -88,7 +113,29 @@ namespace EntityFrameworkCore.RawSQLExtensions.Extensions
 
                     obj = dr.MapObject<T>(colMapping, props);
                     hasResult = true;
-                }     
+                }
+
+            return obj;
+        }
+
+
+        public static T SingleOrDefault<T>(this DbDataReader dr)
+        {
+            var props = typeof(T).GetRuntimeProperties();
+            var colMapping = dr.GetSchema<T>();
+
+            T obj = default(T);
+            bool hasResult = false;
+
+            if (dr.HasRows)
+                while (dr.Read())
+                {
+                    if (hasResult)
+                        throw new InvalidOperationException("Sequence contains more than one matching element");
+
+                    obj = dr.MapObject<T>(colMapping, props);
+                    hasResult = true;
+                }
 
             return obj;
         }
